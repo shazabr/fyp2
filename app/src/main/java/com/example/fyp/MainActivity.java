a2ee49e6dec712b15bf082cf0cab022f0bff00d9
shazabr <shazabrahmed@gmail.com>
2019-02-26 12:48:05 +0000

    login code for admin

---
+++ app/src/main/java/com/example/fyp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.fyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText username;
+    private EditText password;
+    private TextView test;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        username = (EditText)findViewById(R.id.eusername);
+        password = (EditText)findViewById(R.id.epassword);
+
+        test = (TextView) findViewById(R.id.etest);
+        login = (Button) findViewById(R.id.elogin);
+
+
+    }
+
+    login.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View view) {
+            validate(username.getText().toString(), password.getText().toString());
+
+        }
+
+    }
+
+    // adding a admin with specified username and password,
+    // also created a "MainMenu" class for the second homepage that it will direct to
+    // if the username and password match accordingly.
+
+    private void validate(String userName, String userPassword){
+        if ((userName == "admin") && (userPassword == "pass")) {
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+            startActivity(intent);
+        } else {
+            test.setText("wrong password, try again");
+        }
+    }
+}
